a751c6a767f74947904b7af9d2be828dd81eea45
ayden <aydendiel@gmail.com>
2025-08-04 14:17:15 -0400

    feat: implemented very simple markdown parser

--- httputil/stream_reader_response.go
+++ httputil/stream_reader_response.go
@@ -7,32 +7,208 @@ import (
   "log"
   "net/http"
   "bufio"
+  "io"
   "encoding/json"
 )
 
-func renderResponseStream(w http.ResponseWriter, apiResp *http.Response, flusher http.Flusher) {
-  // https://leidnedya.github.io/markweb/#https://daringfireball.net/projects/markdown/syntax#philosophy
+type ApiRespScanner struct {
+  data string
+  i int
+  resp *http.Response
+  internalScanner *bufio.Scanner
+  token string
+}
 
-	scanner := bufio.NewScanner(apiResp.Body)
+func (ars *ApiRespScanner) NextToken() (string, error) {
+  if ars.internalScanner == nil {
+    ars.internalScanner = bufio.NewScanner(ars.resp.Body)
+  }
 
-  // tagStack := make([]string, 0, 20)
-
-	for scanner.Scan() {
-		line := scanner.Text()
-
+  if ars.internalScanner.Scan() {
+		line := ars.internalScanner.Text()
     var partialResponse ReaderResponse
     json.Unmarshal([]byte(line), &partialResponse)
     text := partialResponse.Response
-		fmt.Println("Received:", text)
+    ars.data += text
+  }
 
-    if text == "\n" {
-      fmt.Fprintf(w, "</p><p>")
+  ars.i++
+  if (ars.i >= len(ars.data)) {
+    log.Println(ars.data)
+    return "", io.EOF
+  }
+
+  ars.token = string(ars.data[ars.i])
+  return ars.token, nil
+}
+
+func renderResponseStream(w http.ResponseWriter, apiResp *http.Response, flusher http.Flusher) {
+  scanner := &ApiRespScanner{resp: apiResp}
+
+  tagStack := make([]string, 0, 20)
+
+  ch := ""
+  var err error
+	for err == nil {
+    ch, err = scanner.NextToken()
+    isNewline := false
+
+    // ignore any whitespace immediately following a newline
+    if ch == "\n" {
+      isNewline = true
+      ch, err = scanner.NextToken()
+      for err != nil && (ch == " " || ch == "\t") {
+        ch, err = scanner.NextToken()
+      }
+      if err != nil {
+        break
+      }
+    } 
+
+    if ch == "\n" {
+      if len(tagStack) > 0 {
+        tag := tagStack[len(tagStack) - 1]
+        fmt.Fprintf(w, "</%s>", tag)
+        flusher.Flush()
+        tagStack = tagStack [:len(tagStack) - 1]
+      }
+    } else if ch == "\\" { // escape character
+      ch, err = scanner.NextToken()
+      if err != nil {
+        continue
+      }
+      fmt.Fprintf(w, ch)
+      flusher.Flush()
+    } else if isNewline && (ch == "=" || ch == "-") {
+      for ch == "=" || ch == "-" && err != nil {
+        ch, err = scanner.NextToken()
+      }
+      fmt.Fprintf(w, "\n\t\t<br>\n")
+      flusher.Flush()
+    } else if ch == "[" {
+      if len(tagStack) == 0 {
+        // If there's no current tag, put this in a p tag
+        tagStack = append(tagStack, "p")
+        fmt.Fprintf(w, "<p>")
+        flusher.Flush()
+      }
+      labelText := ""
+      href := ""
+      finishedEarly := false
+
+      // parse label section
+      for true {
+        ch, err = scanner.NextToken()
+        if err != nil {
+          finishedEarly = true
+          break
+        }
+        if ch == "]" {
+          break
+        }
+        labelText += ch
+      }
+      if finishedEarly {
+        fmt.Fprintf(w, strings.ReplaceAll("[" + labelText, "%", "%%"))
+        flusher.Flush()
+        continue
+      }
+
+      // parse text section
+      ch, err = scanner.NextToken()
+      if ch != "(" {
+        fmt.Fprintf(w, strings.ReplaceAll("[" + labelText + "]", "%", "%%"))
+        flusher.Flush()
+        continue
+      }
+      for true {
+        ch, err = scanner.NextToken()
+        if err != nil {
+          finishedEarly = true
+          break
+        }
+        if ch == ")" {
+          break
+        }
+        href += ch
+      }
+      if finishedEarly {
+        fmt.Fprintf(w, strings.ReplaceAll("[" + labelText + "](" + href, "%", "%%"))
+        flusher.Flush()
+        continue
+      }
+
+      // happy path
+      fmt.Fprintf(w, strings.ReplaceAll("<a href=\"" + href + "\">" + labelText + "</a>", "%", "%%"))
+      flusher.Flush()
+    } else if ch == "!" {
+      ch, err = scanner.NextToken()
+
+      // ignore if not followed by angle bracket
+      if ch != "[" {
+        fmt.Fprintf(w, strings.ReplaceAll("!" + ch, "%", "%%"))
+        flusher.Flush()
+        continue
+      }
+
+      altText := ""
+      src := ""
+      finishedEarly := false
+
+      // parse alt text section
+      for true {
+        ch, err = scanner.NextToken()
+        if err != nil {
+          finishedEarly = true
+          break
+        }
+        if ch == "]" {
+          break
+        }
+        altText += ch
+      }
+      if finishedEarly {
+        fmt.Fprintf(w, strings.ReplaceAll("[" + altText, "%", "%%"))
+        flusher.Flush()
+        continue
+      }
+
+      // parse text section
+      ch, err = scanner.NextToken()
+      if ch != "(" {
+        fmt.Fprintf(w, strings.ReplaceAll("[" + altText + "]", "%", "%%"))
+        flusher.Flush()
+        continue
+      }
+      for true {
+        ch, err = scanner.NextToken()
+        if err != nil {
+          finishedEarly = true
+          break
+        }
+        if ch == ")" {
+          break
+        }
+        src += ch
+      }
+      if finishedEarly {
+        fmt.Fprintf(w, strings.ReplaceAll("[" + altText + "](" + src, "%", "%%"))
+        flusher.Flush()
+        continue
+      }
+
+      // happy path
+      fmt.Fprintf(w, strings.ReplaceAll("<img src=\"" + src + "\" alt=\"" + altText + "\">", "%", "%%"))
       flusher.Flush()
     } else {
-      fmt.Fprintf(w, strings.ReplaceAll(text, "%", "%%")) // escape '%' in backtick string
+      if len(tagStack) == 0 {
+        tagStack = append(tagStack, "p")
+        fmt.Fprintf(w, "<p>\n") // escape '%' in backtick string
+        flusher.Flush()
+      }
+      fmt.Fprintf(w, strings.ReplaceAll(ch, "%", "%%")) // escape '%' in backtick string
       flusher.Flush()
     }
-
 	}
 }
 
